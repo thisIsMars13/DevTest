c60a544da317251f1c888f010e04b7b4ab826668
thisIsMars13 <jgil@village88.com>
2021-11-08 21:39:09 +0800

    commit untracked files

--- dev_test_frontend/src/App.js
+++ dev_test_frontend/src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Home from './views/HomeView/Home';
+import Login from './views/LoginView/Login';
+import Header from './views/components/Header';
+import Footer from './views/components/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Header />
+      <Container>
+          <Route exact path = '/'><Home/></Route>
+          <Route exact path = '/login'><Login/></Route>
+      </Container>
+      <Footer />
+    </Router>
   );
 }
 
